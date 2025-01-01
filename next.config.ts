94364e1be29d0fa32c766f10f550ca1a08bae601
AsimMahmudov <AsimMahmudov>
2025-01-01 22:19:08 +0600

    start

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+// next.config.js
+module.exports = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "media.tenor.com",
-        port: "",
-        pathname: "**", // Allows all paths on media.tenor.com
-      },
-    ],
+    domains: ['i.gifer.com'],
   },
 };
-
-export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import React from 'react';
  const Home = () => {
   return (
     <div className='home'>
-     <h1>  👋 привет пока тут ничего нет...</h1>
+     <h1>  👋 привет пока тут ничего нет</h1>
 
 <div className="image">
-<Image width={100} height={200} className='img' src="https://media.tenor.com/C5ouH-u6KYAAAAAM/cafe.gif" alt="" />
+<img className='img' src="https://i.gifer.com/embedded/download/WiFj.gif" alt="" />
 
 </div>
     </div>
