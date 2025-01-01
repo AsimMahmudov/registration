1e70cf8e8f64e0a90548d4c4398e4877f8673944
AsimMahmudov <AsimMahmudov>
2025-01-01 22:11:43 +0600

    start2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
- import React from 'react';
+ import Image from 'next/image';
+import React from 'react';
  
  const Home = () => {
   return (
     <div className='home'>
-      👋 привет пока тут ничего нет...
+     <h1>  👋 привет пока тут ничего нет...</h1>
+
+<div className="image">
+<Image width={100} height={200} className='img' src="https://media.tenor.com/C5ouH-u6KYAAAAAM/cafe.gif" alt="" />
+
+</div>
     </div>
   );
  };
